5b0474f7ac05495dc35af74a4c301187cd489784
satya-cheepuri <drsatyagouri@gmail.com>
2022-01-28 18:41:10 -0500

    added ICE 3 Project files from WEBD ICES repo

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,8 +1,152 @@
+// IIFE -- Immediately Invoked Function Express
+// AKA anonymous self-executing function
+
+"use strict";
 (function()
 {
+    function DisplayHome()
+    {
+        console.log("Home Page");
+
+        let AboutUsButton = document.getElementById("AboutUsButton");
+        AboutUsButton.addEventListener("click", function()
+        {
+            location.href = "about.html";
+        });
+
+        // Step 1 - get a reference to an entry point(s) (insertion / deletion point)
+        let MainContent = document.getElementsByTagName("main")[0];
+        let DocumentBody = document.body;
+        
+        // Step 2 - Create a HTML Element in memory
+        let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
+
+        // Step 3 - Configure new Element
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+        let FirstString = "This is";
+        let SecondString = `${FirstString} the Main Paragraph`;
+        MainParagraph.textContent = SecondString;
+        Article.setAttribute("class", "container");
+
+
+        // Step 4 - perform insertion / deletion
+
+        // example of insert after (append)
+        MainContent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
+
+    }
+
+    function DisplayAboutPage()
+    {
+        console.log("About Us Page");
+    }
+
+    function DisplayProjectsPage()
+    {
+        console.log("Our Projects Page");
+    }
+
+    function DisplayServicesPage()
+    {
+        console.log("Our Services Page");
+    }
+
+    function DisplayContactPage()
+    {
+        console.log("Contact Us Page");
+
+        let sendButton = document.getElementById("sendButton");
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox");
+
+        sendButton.addEventListener("click", function()
+        {
+            if(subscribeCheckbox.checked)
+            {
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                if(contact.serialize())
+                {
+                    let key = contact.FullName.substring(0, 1) + Date.now();
+
+                    localStorage.setItem(key, contact.serialize());
+                }
+            }
+        });
+    }
+
+    function DisplayContactListPage()
+    {
+        console.log("Contact-List Page");
+        if(localStorage.length > 0)
+        {
+            let contactList = document.getElementById("contactList");
+
+            let data = ""; // data container -> add deserialized data from the localStorage
+
+            let keys = Object.keys(localStorage); // returns a string array of keys
+
+            let index = 1; // counts how many keys
+
+            // for every key in the keys array (collection), loop
+            for (const key of keys) 
+            {
+                let contactData = localStorage.getItem(key); // get localStorage data value related to the key
+
+                let contact = new Contact(); // create a new empty contact object
+                contact.deserialize(contactData);
+
+                // inject a repeatable row into the contactList
+                data += `<tr>
+                <th scope="row" class="text-center">${index}</th>
+                <td>${contact.FullName}</td>
+                <td>${contact.ContactNumber}</td>
+                <td>${contact.EmailAddress}</td>
+                <td></td>
+                <td></td>
+                </tr>
+                `;
+
+                index++;
+            }
+
+            contactList.innerHTML = data;
+        }
+    }
+
+
+    // named function
     function Start()
     {
-        console.log("App Started!");
+        console.log("App Started!!");
+
+        switch (document.title) {
+          case "Home":
+            DisplayHome();
+            break;
+          case "About Us":
+            DisplayAboutPage();
+            break;
+          case "Our Projects":
+            DisplayProjectsPage();
+            break;
+          case "Our Services":
+            DisplayServicesPage();
+            break;
+          case "Contact-List":
+            DisplayContactListPage();
+            break;
+          case "Contact Us":
+            DisplayContactPage();
+            break;
+        }
     }
+    
+
     window.addEventListener("load", Start);
+
+
 })();
---
+++ Scripts/contact.js
@@ -0,0 +1,69 @@
+"use strict";
+class Contact
+{
+    // getters and setters
+    get FullName()
+    {
+        return this.m_fullName;
+    }
+
+    set FullName(full_name)
+    {
+        this.m_fullName = full_name;
+    }
+
+    get ContactNumber()
+    {
+        return this.m_contactNumber;
+    }
+
+    set ContactNumber(contact_number)
+    {
+        this.m_contactNumber = contact_number;
+    }
+
+    get EmailAddress()
+    {
+        return this.m_emailAddress;
+    }
+
+    set EmailAddress(email_address)
+    {
+        this.m_emailAddress = email_address;
+    }
+
+    // constructor
+    constructor(fullName = "", contactNumber = "", emailAddress = "")
+    {
+        this.FullName = fullName;
+        this.ContactNumber = contactNumber;
+        this.EmailAddress = emailAddress;
+    }
+
+    // public utility methods
+
+    serialize()
+    {
+        if(this.FullName !== "" && this.ContactNumber !== "" && this.EmailAddress !== "")
+        {
+            return `${this.FullName},${this.ContactNumber},${this.EmailAddress}`;
+        }
+        console.error("One or more properties of the Contact Object are missing or invalid");
+        return null;
+    }
+
+    deserialize(data) // assume that data is in a comma-separated format (string array of properties)
+    {
+        let propertyArray = data.split(",");
+        this.FullName = propertyArray[0];
+        this.ContactNumber = propertyArray[1];
+        this.EmailAddress = propertyArray[2];
+    }
+
+    // overridden methods
+
+    toString()
+    {
+        return `Full Name: ${this.FullName} \nContact Number: ${this.ContactNumber} \nEmail Address: ${this.EmailAddress}`;
+    }
+}
